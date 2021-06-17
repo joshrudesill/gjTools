ae55576c4943357fd18568fa8de58a36659bd46b
Josh Rudesill <joshrudesill@gmail.com>
2021-06-17 09:10:10 -0500

    GetSmallest v1

--- Testing/JTest.cs
+++ Testing/JTest.cs
@@ -1,6 +1,8 @@
 ﻿using Rhino;
 using Rhino.Commands;
-
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
 namespace gjTools
 {
     public class JTest : Command
@@ -15,8 +17,16 @@ namespace gjTools
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            var h = new DialogTools(doc);
-            h.updateUserInfo();
+            Rhino.Input.Custom.GetObject go = new Rhino.Input.Custom.GetObject();
+            go.SetCommandPrompt("Select objects to get smallest rotation..");
+            go.Get();
+            var ro = go.Object(0);
+            var vec = new Point3d(5, 5, 0);
+            var xf = Transform.Rotation(Math.PI, vec);
+            Guid id = doc.Objects.Transform(ro, xf, false);
+
+
+            doc.Views.Redraw();
             return Result.Success;
         }
     }
