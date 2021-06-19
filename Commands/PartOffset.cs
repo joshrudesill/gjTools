e4767ed1e31789243343362d71e7e6776baa1457
MrFlat8 <mrflat8@gmail.com>
2021-06-19 16:25:23 -0500

    part offset

--- Commands/PartOffset.cs
+++ Commands/PartOffset.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using Rhino;
 using Rhino.Commands;
+using Rhino.Input.Custom;
 using System.Collections.Generic;
 using Rhino.Geometry;
+
 namespace gjTools.Commands
 {
     public class PartOffset : Command
@@ -15,37 +17,80 @@ namespace gjTools.Commands
         public override string EnglishName => "PartOffset";
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            DialogTools d = new DialogTools(doc);
-            Rhino.Input.Custom.GetObject go = new Rhino.Input.Custom.GetObject();
-            go.SetCommandPrompt("Select object(s) to offset..");
-            Rhino.Input.GetResult gr = go.GetMultiple(0, -1);
-            if (gr != Rhino.Input.GetResult.Object)
+            var go = new GetObject();
+                go.SetCommandPrompt("Select Objects to Offset");
+                go.GeometryFilter = Rhino.DocObjects.ObjectType.Curve;
+                go.AlreadySelectedObjectSelect = true;
+                go.GetMultiple(1, 0);
+
+            if (go.CommandResult() != Result.Success)
+                return go.CommandResult();
+
+            var obj = new List<Rhino.DocObjects.ObjRef>();
+            foreach (var o in go.Objects())
+                obj.Add(o);
+
+            var options = new List<string> { "plainFilm", "Printed", "Router" };
+            var optiVal = new List<double> { 0.125, 0.25, 0.5 };
+            var offset = 0.125;
+            var gs = new GetString();
+                gs.SetCommandPrompt("Offset Amount or Specify");
+                gs.AcceptNumber(true, true);
+
+            foreach (var o in options)
+                gs.AddOption(o);
+
+                gs.SetDefaultString(options[0]);
+                gs.SetCommandPromptDefault(options[0]);
+            var gType = gs.Get();
+
+            // see what the shit-head typed
+            if (gs.CommandResult() != Result.Success)
+                return Result.Cancel;
+
+            if (gType == Rhino.Input.GetResult.String)
             {
-                RhinoApp.WriteLine("No objects selected. Command canceled");
+                if (options.Contains(gs.StringResult().Trim()))
+                {
+                    offset = optiVal[options.IndexOf(gs.StringResult().Trim())];
+                }
+                else
+                {
+                    RhinoApp.WriteLine("Not a Number, try again...");
+                    return Result.Cancel;
+                }
+            }
+            else if (gType == Rhino.Input.GetResult.Number)
+            {
+                offset = gs.Number();
+            } else
+            {
                 return Result.Cancel;
             }
-            List<Rhino.DocObjects.RhinoObject> ids = new List<Rhino.DocObjects.RhinoObject>();
-            double offset = 0.125;
-            Rhino.Input.RhinoGet.GetNumber("Offset Distance?", true, ref offset);
-            var layer = doc.Layers.FindName("Temp");
-            int li;
-            if (layer == null)
+
+            var lt = new LayerTools(doc);
+            var tmplayer = lt.CreateLayer("Temp", System.Drawing.Color.Bisque);
+
+            foreach(var o in obj)
             {
-                li = d.addLayer("Temp", System.Drawing.Color.FromArgb(135, 0, 255, 21));
+                var offCrv = o.Curve().Offset(new Point3d(-10000, -10000, 0),
+                    new Vector3d(0,0,1),
+                    offset,
+                    0.01,
+                    CurveOffsetCornerStyle.Round);
+                foreach (var oo in offCrv)
+                {
+                    Guid obGUID = doc.Objects.AddCurve(oo);
+                    lt.AddObjectsToLayer(obGUID, tmplayer);
+                }
             }
-            else
-            {
-                li = layer.Index;
-            }
-            doc.Layers.SetCurrentLayerIndex(li, true);
-            Curve[] cl;
-            for (int i = 0; i < go.ObjectCount; i++)
-            {
-                cl = go.Object(i).Curve().Offset(Plane.WorldXY, offset, 0.001, CurveOffsetCornerStyle.Round);
-                doc.Objects.AddCurve(cl[0]);
-            }
+
+            RhinoApp.WriteLine("Number Chosen: " + offset);
+
             doc.Views.Redraw();
             return Result.Success;
         }
+
+        
     }
 }
