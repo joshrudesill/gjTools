5e85cf5706f77dd964a2be7338db03291c68a8db
Josh Rudesill <joshrudesill@gmail.com>
2021-06-24 12:58:59 -0500

    cut type

--- Commands/BatchKerf.cs
+++ Commands/BatchKerf.cs
@@ -2,7 +2,7 @@
 using Rhino;
 using Rhino.Commands;
 using System.Collections.Generic;
-
+using Rhino.Geometry;
 namespace gjTools.Commands
 {
     public class BatchKerf : Command
@@ -14,27 +14,37 @@ namespace gjTools.Commands
         public struct LayerRhObjList
         {
             public Rhino.DocObjects.Layer rLayer;
-            public List<Rhino.DocObjects.Layer> cLayers;
+            public Rhino.DocObjects.Layer cLayers;
             public Rhino.DocObjects.RhinoObject[] oList;
-            public LayerRhObjList(Rhino.DocObjects.Layer rLayer, Rhino.DocObjects.RhinoObject[] oList, List<Rhino.DocObjects.Layer> cLayers)
+            public LayerRhObjList(Rhino.DocObjects.Layer rLayer, Rhino.DocObjects.RhinoObject[] oList, Rhino.DocObjects.Layer cLayers)
             {
                 this.rLayer = rLayer;
                 this.oList = oList;
                 this.cLayers = cLayers;
             }
         }
+        public struct EmbeddedLO
+        {
+            public Rhino.DocObjects.Layer pLayer;
+            public List<LayerRhObjList> rl;
+            public EmbeddedLO(Rhino.DocObjects.Layer pLayer, List<LayerRhObjList> rl)
+            {
+                this.pLayer = pLayer;
+                this.rl = rl;
+            }
+        }
         public static BatchKerf Instance { get; private set; }
 
-        public override string EnglishName => "BatchKerf";
+        public override string EnglishName => "gjBatchKerf";
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
             DialogTools d = new DialogTools(doc);
             LayerTools lt = new LayerTools(doc);
-            var go = d.selectObjects("");
             var parents = lt.getAllParentLayers();
             var parentsString = new List<string>();
             var layerobj = new List<LayerRhObjList>();
+            var embeddedl = new List<EmbeddedLO>();
             foreach (var p in parents)
             {
                 parentsString.Add(p.Name);
@@ -43,12 +53,46 @@ namespace gjTools.Commands
             parents.Clear();
             foreach(var i in la)
             {
-                parents.Add(doc.Layers[doc.Layers.FindByFullPath(i, -1)]);
+                var path = doc.Layers.FindByFullPath(i, -1);
+                parents.Add(doc.Layers[path]);
             }
             foreach (var ly in parents)
             {
-                layerobj.Add(new LayerRhObjList(ly, doc.Objects.FindByLayer(ly), lt.getAllCutLayers(ly, false)));
+                var eo = new EmbeddedLO();
+                layerobj = new List<LayerRhObjList>();
+                var clayers = lt.getAllCutLayers(ly, false);
+                if (clayers == null)
+                {
+                    RhinoApp.WriteLine("FAILED: No cut layers on one or more layers!");
+                    return Result.Failure;
+                }
+                foreach (var cl in clayers)
+                {
+                    layerobj.Add(new LayerRhObjList(ly, doc.Objects.FindByLayer(cl), cl));
+                }
+                eo = new EmbeddedLO(ly, layerobj);
+                embeddedl.Add(eo);
             }
+            string kerf = "";
+            foreach (var lo in embeddedl)
+            {
+                BoundingBox bb;
+                Rhino.DocObjects.RhinoObject.GetTightBoundingBox(new List<Rhino.DocObjects.RhinoObject>(doc.Objects.FindByLayer(lo.pLayer)), out bb);
+                foreach (var rl in lo.rl)
+                {
+                    kerf += rl.cLayers.Name + ": ";
+                    int length = 0;
+                    foreach (var cr in rl.oList)
+                    {
+                        length += (int)new Rhino.DocObjects.ObjRef(cr).Curve().GetLength();
+                    }
+                    kerf += length.ToString() + "\n";
+                }
+                Plane plane = new Plane();
+                plane.Origin = bb.GetCorners()[2];
+                doc.Objects.AddText(kerf, plane, 1.0, "Arial", false, false, TextJustification.BottomRight);
+            }
+            RhinoApp.WriteLine(kerf);
             return Result.Success;
         }
     }
