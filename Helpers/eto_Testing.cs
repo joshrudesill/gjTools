a0e58ced8d0b62201ff3488c3d97a438445e954d
Greg Callahan <mrflat8@gmail.com>
2021-07-22 16:44:37 -0500

    ssss

--- Helpers/eto_Testing.cs
+++ Helpers/eto_Testing.cs
@@ -5,16 +5,19 @@ using Rhino;
 
 namespace gjTools.Helpers
 {
-    class AssignCutTypeForm
+    class DualListDialog
     {
         public RhinoDoc doc;
-        public List<string> LeftList;
-        public AssignCutTypeForm(RhinoDoc docum)
+        
+        public DualListDialog(RhinoDoc docum)
         {
             doc = docum;
         }
         public void Makeform()
         {
+            var leftList = new LayerTools(doc).getAllParentLayersStrings();
+            var rightList = leftList;
+
             var window = new Dialog()
             {
                 ClientSize = new Eto.Drawing.Size(515, 515),
@@ -29,20 +32,52 @@ namespace gjTools.Helpers
                 Height = 350,
                 Width = 240
             };
-            optionList.DataStore = new LayerTools(doc).getAllParentLayersStrings();
+            var lt = new LayerTools(doc).getAllParentLayersStrings();
+            optionList.DataStore = leftList;
+
+            layout.AddRow(optionList, MultiList(rightList));
+
+            var okButt = new Button()
+            {
+                Text = "Ok",
+                Width = 40,
+                Height = 25
+            };
+            //okButt.Click += OkButtonPressed();
+
+            window.Content = layout;
+            window.ShowModal();
+        }
+
+        private void OkButtonPressed()
+        {
+            var vals = new List<string>();
+
+            
+        }
+
+        private GridView MultiList(List<string> input)
+        {
+            var newMultiList = new List<List<string>>();
+            foreach(var s in input)
+                newMultiList.Add(new List<string>{ s });
 
             var multiList = new GridView()
             {
                 Height = 350,
                 Width = 240,
+                ShowHeader = false,
+                AllowMultipleSelection = true,
+                DataStore = newMultiList
             };
-            multiList.DataStore = new LayerTools(doc).getAllParentLayersStrings();
+            multiList.Columns.Add(new GridColumn()
+            {
+                Editable = false,
+                DataCell = new TextBoxCell(0),
+                Width = 220
+            });
 
-            layout.AddRow(optionList, multiList);
-
-
-            window.Content = layout;
-            window.ShowModal();
+            return multiList;
         }
     }
 }
