1980a218c54129f9d043c41c85f3eea4d0ad06de
Josh Rudesill <joshrudesill@gmail.com>
2022-03-30 10:28:22 -0500

    zund eyes bug

--- Commands/ZundEyes.cs
+++ Commands/ZundEyes.cs
@@ -154,6 +154,11 @@ namespace gjTools.Commands
                     {
                         Circle c2 = new Circle(first, 0.125);
                         cl.Add(c2.ToNurbsCurve());
+                    } 
+                    else if (j == 0 && numEyesS == 1)
+                    {
+                        Circle c2 = new Circle(first, 0.125);
+                        cl.Add(c2.ToNurbsCurve());
                     }
                     
                 }
